c93365f79fc810d2ecbfc58c7e33d068330e5ce7
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-12-19 16:18:22 +0100

    Build edit feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
@@ -7,6 +7,8 @@ function App() {
   const [fileName, setfileName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [savedSuccessfully, setSavedSuccessfully] = useState<boolean>();
+
+  const [filesList, setFilesList] = useState(null);
 
   const handleWrite = async () => {
     setIsLoading(true);
@@ -24,11 +26,46 @@ function App() {
       }, 2000);
       setIsLoading(false);
     }
+    fetchFiles();
   };
-  console.log(content);
+
+  const fetchFiles = async () => {
+    const response = await fetch("http://localhost:3000/getDocs", {
+      method: "get",
+    });
+    const data = await response.json();
+    setFilesList(data);
+  };
+  useEffect(() => {
+    fetchFiles();
+  }, []);
+
+  const fetchFile = async (fileName: string) => {
+    const response = await fetch(`http://localhost:3000/getDoc${fileName}`, {
+      method: "get",
+    });
+    const data = await response.json();
+    setContent(data.content);
+  };
+
   return (
     <>
-      <ReactQuill theme="snow" value={content} onChange={setContent} />
+      <select
+        name="pets"
+        id="pet-select"
+        onInput={(e) => {
+          const fileName = e.target.value;
+          fetchFile(fileName);
+          setfileName(fileName);
+        }}
+      >
+        <option value="">please choose a file</option>
+        {filesList &&
+          filesList.map((item) => {
+            return <option value={item}>{item}</option>;
+          })}
+      </select>
+
       <input
         value={fileName}
         onInput={(e) => setfileName(e.target.value)}
@@ -38,6 +75,7 @@ function App() {
       <button onClick={handleWrite}>Save file to server</button>
       {isLoading && "waiting for the server to respond..."}
       {savedSuccessfully && "saved successfully!"}
+      <ReactQuill theme="snow" value={content} onChange={setContent} />
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -25,6 +25,23 @@ app.post("/writeDoc", async (req, res) => {
   res.send("wrote file successfully!");
 });
 
+app.get("/getDocs", async (req, res) => {
+  let filesList = [];
+  fs.readdir("writtenFiles/", (err, fileNames) => {
+    fileNames.forEach((file) => {
+      filesList.push(file.split(".html")[0]);
+    });
+    console.log(filesList);
+    res.send(filesList);
+  });
+});
+
+app.get("/getDoc:fileName", async (req, res) => {
+  fs.readFile(`writtenFiles/${req.params.fileName}.html`, "utf8", (err, data) =>
+    res.send(JSON.stringify({ content: data }))
+  );
+});
+
 app.get("/get-server-status", (req, res) => {
   res.status(200);
   res.send("server is running");
